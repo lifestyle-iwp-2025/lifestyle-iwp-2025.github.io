e644345b8b64799a77b9cc0d7cc1c822e9864c64
lydiareiter <l.maslowski@students.htl-leonding.ac.at>
2025-05-23 16:23:18 +0200

    feat: add main and sub page components  IWP-21

---
+++ src/app/sub-page/sub-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sub-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './sub-page.component.html',
+  styleUrl: './sub-page.component.css'
+})
+export class SubPageComponent {
+
+}
