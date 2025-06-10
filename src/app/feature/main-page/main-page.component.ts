26e758fce697fbdc6b3cb8980e03a489e819dbbd
lydiareiter <94108398+lydiareiter@users.noreply.github.com>
2025-06-10 22:52:22 +0200

    fix: remove standalone setup

--- src/app/feature/main-page/main-page.component.ts
+++ src/app/feature/main-page/main-page.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-main-page',
-  imports: [],
   templateUrl: './main-page.component.html',
-  standalone: true,
   styleUrl: './main-page.component.css'
 })
 export class MainPageComponent {
--- src/app/feature/sub-page/sub-page.component.ts
+++ src/app/feature/sub-page/sub-page.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sub-page',
-  imports: [],
   templateUrl: './sub-page.component.html',
   styleUrl: './sub-page.component.css'
 })
