a667a989e67721d772c0fdec4690e9f3dc6a9356
lydiareiter <94108398+lydiareiter@users.noreply.github.com>
2025-06-11 13:00:17 +0200

    feat(navbar): add navbar module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { ConfigService } from './config.service';
 import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { NavbarModule } from './navbar/navbar.module';
 
 @NgModule({
-  imports: [HttpClientModule],
+  declarations: [],
+  imports: [HttpClientModule, CommonModule, NavbarModule],
+  exports: [NavbarModule],
   providers: [ConfigService]
 })
 export class CoreModule {}
