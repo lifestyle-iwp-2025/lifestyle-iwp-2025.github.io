d92b8c4533a10d6763e4f116c36273f96410513f
lydiareiter <94108398+lydiareiter@users.noreply.github.com>
2025-06-11 13:51:38 +0200

    Add sturcture-block component and module

---
+++ src/app/feature/sturcture-block/sturcture-block.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sturcture-block',
+  templateUrl: './sturcture-block.component.html',
+  styleUrl: './sturcture-block.component.css'
+})
+export class SturctureBlockComponent {}
